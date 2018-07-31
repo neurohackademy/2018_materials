7f8e19309741d241bf829acb288455f9f787f8cb
Tal Yarkoni <tyarkoni@gmail.com>
2018-07-30 17:57:10 -0700

    ergh

--- push.sh
+++ push.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  git config --global user.email "tyarkoni@gmail.com"
+  git config --global user.name "Tal Yarkoni"
 }
 
 commit_website_files() {
-  git checkout -b master
+  git checkout master
   git add README.md
   git commit --message "Updating README from Travis build: $TRAVIS_BUILD_NUMBER"
 }
